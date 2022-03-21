08eb66d711f611f8a411e1b981ef9ffbaa66daa8
miftah al rasyid <miftahalrasyid@users.noreply.github.com>
2022-03-21 13:13:47 +0700

    test console

--- pages/odc/[...odcId].js
+++ pages/odc/[...odcId].js
@@ -367,6 +367,7 @@ function Odc({
         <script async src="https://telegram.org/js/telegram-widget.js?18" data-telegram-login="miftah1112_bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
         <script type="text/javascript">
         {function onTelegramAuth(user) {
+            console.log(user);
             alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
         }}
         </script>
